b0e2270d8dbfcabbff87e62f014cf670f34c1c23
Cezain Majeed <63138007+cezainmajeed@users.noreply.github.com>
2022-01-08 11:25:54 +0530

    Create 11.Find Missing And Repeating.cpp

---
+++ Goldman-Sachs/11.Find Missing And Repeating.cpp
@@ -0,0 +1,23 @@
+    int *findTwoElement(int *arr, int n) {
+        int *a=new int[2];
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[abs(arr[i])-1]>0)
+            {
+                arr[abs(arr[i])-1]*=-1;
+            }
+            else
+            {
+                a[0]=abs(arr[i]);
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>0)
+            a[1]=i+1;
+        }
+        
+        return a;
+    }
